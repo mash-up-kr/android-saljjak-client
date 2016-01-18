d68d85e8a90b9704fe1fd00e497f7d3217d67037
Minkyu Ko <minkyuko@makeus.com>
2016-01-19 02:02:15 +0900

    Logo DraweeView 추가

--- app/src/main/java/com/ewhappcenter/saljjak/SplashActivity.java
+++ app/src/main/java/com/ewhappcenter/saljjak/SplashActivity.java
@@ -5,15 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.facebook.drawee.view.SimpleDraweeView;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class SplashActivity extends AppCompatActivity {
+
+    @Bind(R.id.splash_dv_logo) SimpleDraweeView dvLogo;
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         ButterKnife.bind(this);
-
-        SimpleDraweeView dvSample;
     }
 }
